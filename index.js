7f124e7792c9abf83809e5a821e3ce7c2242b23b
Daniel <danielpqb@gmail.com>
2022-06-27 14:30:42 -0300

    Facilitando correção do projeto + Bonus

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 function registerUser() {
+    document.querySelector('.lds-spinner').classList.toggle('hide')
+    document.querySelector('.lgName').classList.toggle('hide')
+    document.querySelector('.lgBtn').classList.toggle('hide')
+
+    user = document.querySelector('.lgName input').value
     const register = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', { name: user })
+
     register.catch((e) => {
         if (e.response.status === 400) {
-            user = prompt('Este nome de usuário já está em uso.\nEscolha outro nome.')
-            registerUser()
+            document.querySelector('.loader').classList.toggle('hide')
+            document.querySelector('.lgName').classList.toggle('hide')
+            document.querySelector('.lgBtn').classList.toggle('hide')
+            alert('Este nome de usuário já está em uso.\nEscolha outro nome.')
+        }
+    })
+    register.then(() => {
+        setTimeout(() => {
+            document.querySelector('.login').classList.add('hide')
+        }, 2000)
+    })
+}
+function getParticipants() {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+    promise.then(showParticipants);
+}
+function showParticipants(resp) {
+    document.querySelector('.sbUsers').innerHTML = `
+        <div class="sbUser" data-identifier="participant" onclick="checkUser(this)">
+            <div class="sbUserData">
+                <img src="img/person.svg" alt="">
+                <h1>Todos</h1>
+            </div>
+            <div class="sbUserCheck hide">
+                <img src="img/checkmark.svg" alt="">
+            </div>
+        </div>`
+
+    participants = resp.data
+    participants.forEach((element) => {
+        document.querySelector('.sbUsers').innerHTML += `
+        <div class="sbUser" data-identifier="participant" onclick="checkUser(this)">
+            <div class="sbUserData">
+                <img src="img/person.svg" alt="">
+                <h1>${element.name}</h1>
+            </div>
+            <div class="sbUserCheck hide">
+                <img src="img/checkmark.svg" alt="">
+            </div>
+        </div>`
+    })
+
+    document.querySelectorAll('.sbUserData h1').forEach((element) => {
+        if (element.innerHTML == msgTo) {
+            element.parentNode.parentNode.classList.add('check')
+            element.parentNode.parentNode.querySelector('.hide').classList.remove('hide')
         }
     })
 }
@@ -29,7 +79,7 @@ function showMessages(resp) {
                 break;
 
             case 'private_message':
-                if (element.to === user) {
+                if (element.to === user || element.from === user) {
                     document.querySelector('main').innerHTML += `<div class="message messagePrivate ${ult}"><span>(${element.time}) <strong>${element.from}</strong> reservadamente para <strong>${element.to}:</strong> ${element.text}</span></div>`
                 }
                 break;
@@ -38,19 +88,27 @@ function showMessages(resp) {
     document.querySelector('.ult').scrollIntoView();
 }
 function sendMessage() {
-    let msg = document.querySelector('.writeMessage').querySelector('textarea').value
+    const msg = document.querySelector('.writeMessage textarea').value
     if (msg != '') {
+        console.log(msgType)
+        if (msgType == 'Reservadamente' || msgType == 'private_message') {
+            msgType = 'private_message'
+        }
+        else {
+            msgType = 'message'
+        }
+
         const send = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', {
             from: user,
-            to: "Todos",
+            to: msgTo,
             text: msg,
-            type: "message" // ou "private_message" para o bônus
+            type: msgType // or "private_message"
         })
         send.then(() => {
             getMessages()
         })
 
-        send.catch((err) => {
+        send.catch(() => {
             window.location.reload()
         })
     }
@@ -75,6 +133,9 @@ function checkUser(user) {
     })
     user.querySelector('.sbUserCheck').classList.remove('hide')
     user.classList.add('check')
+
+    msgTo = document.querySelector('.check .sbUserData h1').innerHTML
+    document.querySelector('.writeMessage p').innerHTML = `Enviando para ${msgTo} (${msgType})`
 }
 
 function checkVisibility(visibility) {
@@ -90,18 +151,38 @@ function checkVisibility(visibility) {
     })
     visibility.querySelector('.sbVisibilityCheck').classList.remove('hide')
     visibility.classList.add('check')
+
+    msgType = document.querySelector('.check .sbVisibilityData h1').innerHTML
+    document.querySelector('.writeMessage p').innerHTML = `Enviando para ${msgTo} (${msgType})`
 }
 
-// //Register user
-// let user = prompt('Digite seu nome de usuário.')
-// registerUser()
+//Common
+let msgTo = 'Todos'
+let msgType = 'Público'
 
-// //Get messages
-// let messages
-// getMessages()
+//Register user
+let user
 
-// //Refresh messages every 3 seconds
-// setInterval(getMessages, 3000)
+//Get messages
+let messages
+getMessages()
 
-// //Keep user conected every 5 seconds
-// setInterval(() => { axios.post('https://mock-api.driven.com.br/api/v6/uol/status', { name: user }) }, 5000)
+//Get participants
+getParticipants()
+setInterval(getParticipants, 10000)
+
+//Refresh messages every 3 seconds
+getMessages()
+setInterval(getMessages, 3000)
+
+//Keep user conected every 5 seconds
+setInterval(() => { axios.post('https://mock-api.driven.com.br/api/v6/uol/status', { name: user }) }, 5000)
+
+//Listen for 'ENTER' key while writing
+let msgArea = document.querySelector('.writeMessage textarea')
+msgArea.addEventListener("keypress", function (k) {
+    if (k.key === "Enter") {
+        k.preventDefault();
+        sendMessage();
+    }
+});
